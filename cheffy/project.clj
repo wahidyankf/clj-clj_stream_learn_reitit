68799f16570e9d9f9a15cd37f8345607ed61379d
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-10-02 22:14:25 +0700

    add min-lein-version

--- cheffy/project.clj
+++ cheffy/project.clj
@@ -1,6 +1,7 @@
 (defproject cheffy "0.1.0-SNAPSHOT"
   :description "Cheffy REST API"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [ring "1.8.1"]
                  [integrant "0.8.0"]
